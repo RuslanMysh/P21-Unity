ea93616551ec85f5696c4950f998ae015296e50a
o4thbreaker <04thbreaker@gmail.com>
2026-01-21 19:24:14 +0700

    add interact button

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -18,10 +20,15 @@ public class Player : MonoBehaviour
 
     public bool IsWalking { get; private set; }
 
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
     private void Update()
     {
         HandleMovement();
-        HandleInteractions();
+        //HandleInteractions();
     }
 
     private void HandleInteractions()
@@ -35,15 +42,15 @@ public class Player : MonoBehaviour
         }
 
         float interactDistance = 2f;
-        if (Physics.Raycast(transform.position, lastInteractionDir, 
+        if (Physics.Raycast(transform.position, lastInteractionDir,
             out RaycastHit raycastHit, interactDistance, counterMask))
         {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
+                // Has ClearCounter
                 clearCounter.Interact();
             }
         }
-       
     }
 
     private void HandleMovement()
@@ -99,5 +106,26 @@ public class Player : MonoBehaviour
         }
 
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
+    }
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractionDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractionDir,
+            out RaycastHit raycastHit, interactDistance, counterMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
     }
 }
