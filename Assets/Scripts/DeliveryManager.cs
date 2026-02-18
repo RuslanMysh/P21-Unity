0d51519463aa80e6b8b41920dd13e2932f481b41
o4thbreaker <04thbreaker@gmail.com>
2026-02-18 20:16:15 +0700

    add delievry ui

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,8 +1,13 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+
     public static DeliveryManager Instance { private set; get; }
 
     [SerializeField] private RecipeListSO recipeListSO;
@@ -35,9 +40,10 @@ public class DeliveryManager : MonoBehaviour
 
             if (waitingRecipeSOList.Count < waitingRecipesMax)
             {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+              
                 waitingRecipeSOList.Add(waitingRecipeSO);
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
             }
         }
     }
@@ -76,6 +82,9 @@ public class DeliveryManager : MonoBehaviour
                 {
                     Debug.Log("Игрок доставил нужный рецепт!");
                     waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+
                     return;
                 }
             }
--- Assets/Scripts/UI/DeliveryManagerUI.cs
+++ Assets/Scripts/UI/DeliveryManagerUI.cs
@@ -3,11 +3,29 @@ using UnityEngine;
 public class DeliveryManagerUI : MonoBehaviour
 {
     [SerializeField] private Transform container;
-    [SerializeReference] private Transform recipeTemplate;
+    [SerializeField] private Transform recipeTemplate;
 
     private void Awake()
     {
         recipeTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSpawned += DeliveryManager_OnRecipeSpawned;
+        DeliveryManager.Instance.OnRecipeCompleted += DeliveryManager_OnRecipeCompleted;
+
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeCompleted(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeSpawned(object sender, System.EventArgs e)
+    {
+        UpdateVisual();
     }
 
     private void UpdateVisual()
@@ -22,6 +40,8 @@ public class DeliveryManagerUI : MonoBehaviour
         {
             Transform recipeTransform = Instantiate(recipeTemplate, container);
             recipeTransform.gameObject.SetActive(true);
+            recipeTransform.GetComponent<DeliveryManagerSingleUI>().SetRecipeSO(recipe);
+
         }
     }
 }
