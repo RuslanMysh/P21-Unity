ea8357de525c88852d001ece790651def6ad991d
o4thbreaker <04thbreaker@gmail.com>
2026-02-19 20:46:05 +0700

    add game flow

---
+++ Assets/Scripts/KitchenGameManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class KitchenGameManager : MonoBehaviour
+{
+    public static KitchenGameManager Instance { get; private set; }
+
+   private enum State
+    {
+        WaitingToStart,
+        CountdownToStart,
+        GamePlaying,
+        GameOver
+    }
+
+    private State state;
+    private float waitingToStartTimer = 1f;
+    private float countdownToStartTimer = 3f;
+    private float gamePlayingTimer = 10f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Больше одного GameManager!");
+        }
+
+        state = State.WaitingToStart;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.WaitingToStart:
+                waitingToStartTimer -= Time.deltaTime;
+                if (waitingToStartTimer > 0f)
+                {
+                    state = State.CountdownToStart;
+                }
+                break;
+            case State.CountdownToStart:
+                countdownToStartTimer -= Time.deltaTime;
+                if (countdownToStartTimer < 0f)
+                {
+                    state = State.GamePlaying;
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                if (gamePlayingTimer > 0f)
+                {
+                    state = State.GameOver;
+                }
+                break;
+            case State.GameOver:
+                break;
+        }
+    }
+
+    public bool IsGamePlaying()
+    {
+        return state == State.GamePlaying;
+    }
+}
